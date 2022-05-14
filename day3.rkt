f22a0330360262ad6bb94ff7aef4168d84726b12
hnOsmium0001 <hn.osmium0001@gmail.com>
2022-05-13 21:18:59 -0700

    Add day 3, part 2

--- day3.rkt
+++ day3.rkt
@@ -11,14 +11,24 @@
         [actual-y y])
     (string-ref (list-ref input-map actual-y) actual-x)))
 
-;; Part 1
-(define (solve-part1 x y)
+(define (count-things-along-path x y delta-x delta-y)
   (cond
     [(>= y map-height) 0]
     [else (let ([thing (thing-at input x y)]
-                [subcase-solution (solve-part1 (+ x 3) (+ y 1))])
+                [subcase-solution (count-things-along-path (+ x delta-x) (+ y delta-y) delta-x delta-y)])
             (cond
               [(eq? thing #\.) subcase-solution]
               [(eq? thing #\#) (+ subcase-solution 1)]))]))
+
+;; Part 1
 (define (part1)
-  (solve-part1 0 0))
+  ;; Each time move right 3, down 1
+  (count-things-along-path 0 0 3 1))
+
+;; Part 2
+(define (part2)
+  (* (count-things-along-path 0 0 1 1)
+     (count-things-along-path 0 0 3 1)
+     (count-things-along-path 0 0 5 1)
+     (count-things-along-path 0 0 7 1)
+     (count-things-along-path 0 0 1 2)))
