1743c7fabd22fdcd8d1ccf47c7c2909f538496ae
hnOsmium0001 <hn.osmium0001@gmail.com>
2022-05-14 17:59:28 -0700

    Add day 5, part 2

--- day5.rkt
+++ day5.rkt
@@ -28,3 +28,17 @@
   (let* ([seat-ids (map make-seat-id input)]
          [max-seat-id (argmax identity seat-ids)])
     max-seat-id))
+
+;; Part 2
+(define (consecutive-id? a b)
+  (eq? a (- b 2)))
+
+;; TODO(hnosm) is there a less ugly way to write sliding window?
+(define (part2)
+  (let* ([seat-ids (sort (map make-seat-id input) <)]
+         [from-i 0]
+         [to-i (- (length input) 2)])
+    (for/first ([i (in-inclusive-range from-i to-i)]
+                #:when (consecutive-id? (list-ref seat-ids i)
+                                        (list-ref seat-ids (add1 i))))
+      (+ (list-ref seat-ids i) 1))))
