1f0a3fd10a1cc940405c2f195eddd6375713eaf4
hnOsmium0001 <hn.osmium0001@gmail.com>
2022-05-16 22:41:40 -0700

    Add day 8, part 2 in Racket

--- day8.rkt
+++ day8.rkt
@@ -32,22 +32,26 @@
 
 ;; Execute the instruction at the `pc` location. If this location is visited (has an entry in `visited`),
 ;; return `pc` directly. Otherwise recurse with the updated `pc` value after executing the instruction
-(define (exec-no-repeat visited state pc)
-  (if (vector-ref visited pc)
-      pc
-      (let* ([ins (vector-ref input pc)]
-             [op (car ins)]
-             [arg (cdr ins)])
-        (vector-set! visited pc #t)
-        (match op
-          ["acc"
-           (define acc (machine-state-accumulator state))
-           (set-machine-state-accumulator! state (+ acc arg))
-           (exec-no-repeat visited state (+ pc 1))]
-          ["jmp"
-           (exec-no-repeat visited state (+ pc arg))]
-          ["nop"
-           (exec-no-repeat visited state (+ pc 1))]))))
+;; If `pc` is set to one-past-the-end of the array of instructions after execution (i.e. =(vector-length input)=)
+;; `pc` is returned which will be equal to =(vector-length input)=.
+(define (exec-no-repeat visited state pc [instructions input])
+  (cond
+    [(= pc (vector-length instructions)) pc]
+    [(vector-ref visited pc) pc]
+    [else
+     (let* ([ins (vector-ref instructions pc)]
+            [op (car ins)]
+            [arg (cdr ins)])
+       (vector-set! visited pc #t)
+       (match op
+         ["acc"
+          (define acc (machine-state-accumulator state))
+          (set-machine-state-accumulator! state (+ acc arg))
+          (exec-no-repeat visited state (+ pc 1) instructions)]
+         ["jmp"
+          (exec-no-repeat visited state (+ pc arg) instructions)]
+         ["nop"
+          (exec-no-repeat visited state (+ pc 1) instructions)]))]))
 
 (define (part1)
   (let ([visited (make-vector (vector-length input) #f)]
@@ -55,3 +59,26 @@
     (let ([pc-on-repeat (exec-no-repeat visited state 0)])
       (cons pc-on-repeat
             (machine-state-accumulator state)))))
+
+;; Part 2
+(define (part2)
+  (define instructions (vector-copy input))
+  (define result null)
+  (for ([i (in-range 0 (vector-length input))])
+    (let* ([ins (vector-ref instructions i)]
+           [op (car ins)]
+           [arg (cdr ins)])
+      (if (equal? op "jmp")
+          (let ([visited (make-vector (vector-length instructions) #f)]
+                [state (make-machine-state)])
+            (display i)
+            (display "\n")
+            (vector-set! instructions i (cons "nop" arg))
+            (if (= (vector-length instructions)
+                   (exec-no-repeat visited state 0 instructions))
+                (set! result (cons i
+                                   (machine-state-accumulator state)))
+                null)
+            (vector-set! instructions i (cons "jmp" arg)))
+          null)))
+  result)
