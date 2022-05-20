38789f590cecf440f490bdecdf57e70808617afc
hnOsmium0001 <hn.osmium0001@gmail.com>
2022-05-19 19:54:38 -0700

    Update day 1 nim solution

--- day1.nim
+++ day1.nim
@@ -6,21 +6,36 @@ proc readInput(inputFilePath: string): seq[int] =
 
 var input = readInput "inputs/day1.txt"
 
-# Can't read from global var `expenses`: that's a side effect
-func solvePart1(expenses: seq[int]): int =
+# We could just return seq[int] and do the multiplication outside, but trying out structs!
+type
+  ProblemSolution = object
+    matches: seq[int]
+    product: int
+
+# Can't read from global var `input`: that's a side effect
+func solvePart1(expenses: seq[int]): ProblemSolution =
   for i in 0 ..< expenses.len:
     for j in (i + 1) ..< expenses.len:
       if expenses[i] + expenses[j] == 2020:
-        return expenses[i] * expenses[j]
-  return 0
+        let product = expenses[i] * expenses[j]
+        return ProblemSolution(matches : @[expenses[i], expenses[j]],
+                               product : product)
+  return ProblemSolution(matches : @[],
+                         product : 0)
 
-func solvePart2(expenses: seq[int]): int =
+func solvePart2(expenses: seq[int]): ProblemSolution =
+  # `result` is implicitly returned, and automatically set with initial values
   for i in 0 ..< expenses.len:
     for j in (i + 1) ..< expenses.len:
       for k in (j + 1) ..< expenses.len:
         if expenses[i] + expenses[j] + expenses[k] == 2020:
-          return expenses[i] * expenses[j] * expenses[k]
-  return 0
+          # Slightly differnet way to write it
+          result.matches = @[expenses[i] * expenses[j] * expenses[k]]
+          result.product = expenses[i] * expenses[j] * expenses[k]
 
-echo solvePart1(input)
-echo solvePart2(input)
+proc printSolution(sol: ProblemSolution) =
+  # TODO print seq
+  echo sol.product
+
+printSolution solvePart1(input)
+printSolution solvePart2(input)
