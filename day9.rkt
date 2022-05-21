017e41b793c220ec744c8e4f33f7d6e65691bdda
hnOsmium0001 <hn.osmium0001@gmail.com>
2022-05-20 20:14:58 -0700

    Cleanup day 9 Racket code

--- day9.rkt
+++ day9.rkt
@@ -2,11 +2,7 @@
 
 (require racket/file)
 
-;; Problem constants
-(define input-file "inputs/day9.txt")
-(define premable-size 25)
-
-(define input (map string->number (file->lines input-file)))
+(define input (map string->number (file->lines "inputs/day9.txt")))
 
 (define (rotate! buffer)
   (let aux ([i 1])
@@ -27,47 +23,19 @@
             [b buffer])
     (= (+ a b) num)))
 
-(define (make-premable-buffer all)
-  (for/vector #:length premable-size ([elm all])
+(define (list->vector/sized lst size)
+  (for/vector #:length size ([elm lst])
     elm))
 
-(define (find-invalid-num)
-  (let* ([buffer (make-premable-buffer input)])
+(define (find-invalid-num entries premable-size)
+  (let ([buffer (list->vector/sized entries premable-size)])
     (for/first ([n (drop input premable-size)]
                 #:when (let ([valid (permutable-from-buffer? buffer n)])
                          (push-into-buffer! buffer n)
                          (not valid)))
       n)))
 
-(define (part1) (find-invalid-num))
-
-;; NOTE: unused, untested
-(define (list/for-subsequence search-space callback)
-  ;; This operation is O(n) to length of search-space
-  (define search-space-len (length search-space))
-  (let impl ([lst search-space]
-             [len 1])
-    ;; Send current subsequence to callback
-    (callback lst len)
-    ;; Process subsequences starting at the same place, with shorter lengths
-    (let loop ([sublen (sub1 len)])
-      (if (> len 0)
-          (begin
-            (impl lst sublen)
-            (loop (sub1 sublen)))
-          null))
-    ;; Process subsequences starting after the current location
-    (impl (cdr lst) (sub1 len))))
-
-;; NOTE: unused, untested
-(define (list/search-subsequence search-space predicate)
-  (define result (cons '() 0))
-  (list/for-subsequence search-space
-                   (lambda (lst len)
-                     (if (predicate lst len)
-                         (set! result (cons lst len))
-                         null)))
-  result)
+(define (part1) (find-invalid-num input 25))
 
 (define (vector-set-last! vec n)
   (vector-set! vec (- (vector-length vec) 1) n))
@@ -111,34 +79,27 @@
   (let impl ([beg 0]
              [end arr-len])
     (callback beg end)
-    (if (>= (- end beg) 2)
+    (when (>= (- end beg) 2)
         ;; If the range length is >= 2...
         (begin
           ;; Process subsequences starting at the same place, with shorter lengths
-          (let loop ([subseq-end (sub1 end)])
-            (if (> (- subseq-end beg) 0)
-                (begin
-                  (callback beg subseq-end)
-                  (loop (sub1 subseq-end)))
-                null))
+          (for ([subseq-end (in-range end beg -1)])
+            (callback beg subseq-end))
           ;; Process subsequences starting right after current =beg=
-          (impl (+ beg 1) end))
-        ;; Else, the range is too small to have any subsequences
-        null)))
+          (impl (+ beg 1) end)))))
 
 (define (slice lst beg end)
   (take (drop lst beg) (- end beg)))
 
 (define (part2)
-  (let ([i.n. (find-invalid-num)]
+  (let ([i.n. (find-invalid-num input 25)]
         [p.s. (list->prefix-sum input)])
     (for-range-pair (lambda (beg end)
-                      (if (and (>= (- end beg) 2)
-                               (= (query-prefix-sum p.s. beg end) i.n.))
-                          (let* ([c (search-min-max (slice input beg end))]
-                                 [min (car c)]
-                                 [max (cdr c)])
-                            ;; NOTE: racket also supports the more traditiona ~% for line breaks
-                            (printf "min: ~a, max: ~a, min+max: ~a\n" min max (+ min max)))
-                          null))
+                      (when (and (>= (- end beg) 2)
+                                 (= (query-prefix-sum p.s. beg end) i.n.))
+                            (let* ([c (search-min-max (slice input beg end))]
+                                   [min (car c)]
+                                   [max (cdr c)])
+                              ;; NOTE: racket also supports the more traditiona ~% for line breaks
+                              (printf "min: ~a, max: ~a, min+max: ~a\n" min max (+ min max)))))
                     (length input))))
